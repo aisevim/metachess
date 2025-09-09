64df648573ba8768dbde4baf16626fefd3d32608
aisevim <a.aydnlp.pro@gmail.com>
2025-09-10 00:09:38 +0200

    feat(Queen): create piece Queen

---
+++ src/pieces/Queen.ts
@@ -0,0 +1,14 @@
+import { SlidingPiece } from '../pieces/SlidingPiece'
+
+export class Queen extends SlidingPiece {
+  directions = [
+    { dx: 1, dy: 0 },
+    { dx: -1, dy: 0 },
+    { dx: 0, dy: 1 },
+    { dx: 0, dy: -1 },
+    { dx: 1, dy: 1 },
+    { dx: 1, dy: -1 },
+    { dx: -1, dy: 1 },
+    { dx: -1, dy: -1 },
+  ]
+}
